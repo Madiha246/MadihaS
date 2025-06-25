97b4d070734103827e1fb6d77305753b10389b90
Mohammed <muthahir26.mm@gmail.com>
2025-06-25 16:44:33 +0530

    meeeeeeeeee

---
+++ Shanamfrm/src/test/java/OrganizationTests/ToCreateOrganizationTest.java
@@ -0,0 +1,10 @@
+package OrganizationTests;
+
+public class ToCreateOrganizationTest {
+
+	public static void main(String[] args) {
+		System.out.println("org craeted");
+
+	}
+
+}
---
+++ Shanamfrm/src/test/java/contactsTest/ToCreateContactTest.java
@@ -0,0 +1,10 @@
+package contactsTest;
+
+public class ToCreateContactTest {
+
+	public static void main(String[] args) {
+		System.out.println("contact created");
+
+	}
+
+}
